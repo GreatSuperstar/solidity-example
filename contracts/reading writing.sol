535cf2716fe45de660b93f8df5902eb6f5113e7b
Macj0427 <macj0427@gmail.com>
2022-06-08 14:44:26 -0700

    R/W to a State Variable

--- contracts/reading writing.sol
+++ contracts/reading writing.sol
@@ -2,11 +2,13 @@ pragma solidity ^0.8.13;
 
 contract SimpleStorage {
     uint public num;
-    function set(uint _num) public  {
-        num = _num
+
+    function set(uint _num) public {
+        num = _num;
     }
 
     function get() public view returns (uint) {
         return num;
     }
 }
+    
