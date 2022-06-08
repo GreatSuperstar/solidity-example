c4ba32018a575f7b6ff25f6790910a1d1a153da4
Macj0427 <macj0427@gmail.com>
2022-06-08 12:24:54 -0700

    over comlelte}

---
+++ contracts/reading writing.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.13;
+
+contract SimpleStorage {
+    uint public num;
+    function set(uint _num) public  {
+        num = _num
+    }
+
+    function get() public view returns (uint) {
+        return num;
+    }
+}
