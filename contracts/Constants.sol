b6b65d2f4173b765c2686801e8a7febaf9e55445
Macj0427 <macj0427@gmail.com>
2022-06-08 12:03:26 -0700

    variables and immutable

--- contracts/Constants.sol
+++ contracts/Constants.sol
@@ -4,3 +4,4 @@ contract Constants {
     address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
     uint public constant MY_UINT = 123;
 }
+
---
+++ contracts/Immutable.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.13;
+
+
+contract Immutable {
+    address public immutable MY_ADDRESS;
+    uint public immutable MY_UINT;
+
+    constructor(uint _myUint) {
+        MY_ADDRESS = msg.sender;
+        MY_UINT = _myUint;
+    }
+}
